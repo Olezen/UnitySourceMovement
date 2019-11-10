401a31b2c9967920e097aa58330e21251c6609a4
Olezen <ole.e.farner@gmail.com>
2019-11-10 12:28:57 +0100

    Add files via upload

---
+++ Modified fragsurf/Ladder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    // Empty class for defining objects as ladders
+    // Probably inefficient, but I didn't want to use tags or layers
+
+}
